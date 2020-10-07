edc672598e022a34b436defe949e267b967dc0c9
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-07 21:57:17 +0300

    feat: add solution Task12

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task12.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task12.java
@@ -52,5 +52,71 @@ public class Task12 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        String chineseZodiac= "Chinese Zodiac: undefined" ;
+        if ((year - 2001) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Snake";
+        if ((year - 2002) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Horse";
+        if ((year - 2003) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Ram";
+        if ((year - 2004) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Monkey";
+        if ((year - 2005) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Rooster";
+        if ((year - 2006) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Dog";
+        if ((year - 2007) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Pig";
+        if ((year - 2008) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Rat";
+        if ((year - 2009) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Ox";
+        if ((year - 2010) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Tiger";
+        if ((year - 2011) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Rabbit";
+        if ((year - 2012) % 12 == 0 ) chineseZodiac = "Chinese Zodiac: Dragon";
+
+        switch (month)
+        {
+            case 1:
+                if (day <= 20) System.out.println("Zodiac Sign: Goat. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Water-bearer. "+chineseZodiac);
+                break;
+            case 2:
+                if (day <= 19) System.out.println("Zodiac Sign: Water-bearer. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Fish. "+chineseZodiac);
+                break;
+            case 3:
+                if (day <= 20) System.out.println("Zodiac Sign: Fish. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Ram. "+chineseZodiac);
+                break;
+            case 4:
+                if (day <= 20) System.out.println("Zodiac Sign: Ram. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Bull. "+chineseZodiac);
+                break;
+            case 5:
+                if (day <= 20) System.out.println("Zodiac Sign: Bull. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Twins. "+chineseZodiac);
+                break;
+            case 6:
+                if (day <= 21) System.out.println("Zodiac Sign: Twins. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Crab. "+chineseZodiac);
+                break;
+            case 7:
+                if (day <= 22) System.out.println("Zodiac Sign: Crab. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Lion. "+chineseZodiac);
+                break;
+            case 8:
+                if (day <= 22) System.out.println("Zodiac Sign: Lion. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Maiden. "+chineseZodiac);
+                break;
+            case 9:
+                if (day <= 21) System.out.println("Zodiac Sign: Maiden. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Scales. "+chineseZodiac);
+                break;
+            case 10:
+                if (day <= 22) System.out.println("Zodiac Sign: Scales. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Scorpion. "+chineseZodiac);
+                break;
+            case 11:
+                if (day <= 22) System.out.println("Zodiac Sign: Scorpion. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Archer. "+chineseZodiac);
+                break;
+            case 12:
+                if (day <= 21) System.out.println("Zodiac Sign: Archer. "+chineseZodiac);
+                else System.out.println("Zodiac Sign: Goat. "+chineseZodiac);
+                break;
+        }
     }
 }
