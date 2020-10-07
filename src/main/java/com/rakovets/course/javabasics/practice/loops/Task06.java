11917b6a93a7c16cf2f9481ede45b70428907021
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-08 02:34:49 +0300

    feat: add solution Task06

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
@@ -23,5 +23,33 @@ public class Task06 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        boolean minus = false;
+        if(amount<0)
+        {
+            minus = true;
+            amount = -amount;
+        }
+        String strAmount = Long.toString(amount);
+        String strReturn = "";
+        int count = strAmount.length()/3;
+        int div= strAmount.length() % 3;
+        if ( div == 0 )
+        {
+            strReturn = strAmount.substring(0,3)+" ";
+            strAmount = strAmount.substring(3,strAmount.length());
+        }
+        else
+        {
+            strReturn = strAmount.substring(0,div)+" ";
+            strAmount=strAmount.substring(div,strAmount.length());
+        }
+        for ( int i = 0;i < strAmount.length()/3;i++)
+        {
+            int idx = i*3;
+            strReturn =  strReturn + strAmount.substring(idx,idx+3)+" ";
+        }
+        if (minus == true)  System.out.print("-"+strReturn);
+        else System.out.print(strReturn);
     }
 }
