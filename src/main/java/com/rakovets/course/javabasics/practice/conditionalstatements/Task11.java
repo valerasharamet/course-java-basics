78a628631c89a48f3262ea073f8d6571962d3a9b
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-08 23:14:04 +0300

    feat: solution Task11

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -29,5 +29,16 @@ public class Task11 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        if (Character.isDigit(letter)) {
+            System.out.print("digit");
+        } else if (Character.isAlphabetic(letter)) {
+            if (Character.UnicodeBlock.of(letter).equals(Character.UnicodeBlock.CYRILLIC)) {
+                System.out.print("cyrillic");
+            } else if (Character.UnicodeBlock.of(letter).equals(Character.UnicodeBlock.BASIC_LATIN)){
+                System.out.print("latin");
+            }
+        }
+        else System.out.print("undefined");
     }
 }
