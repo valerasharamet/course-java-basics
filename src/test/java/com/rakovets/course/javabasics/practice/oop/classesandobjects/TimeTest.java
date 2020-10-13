294ce03a81c801cd1e418090d9abe3cf2c976079
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-13 20:20:25 +0300

    feat: solution Task01

--- src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/TimeTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/TimeTest.java
@@ -2,6 +2,8 @@ package com.rakovets.course.javabasics.practice.oop.classesandobjects;
 import java.util.Scanner;
 public class TimeTest {
     public static void main(String[] args) {
-        int h = Integer.parseInt(args[0]);
+        Scanner scanner = new Scanner();
+
+        int h =  (args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[2]));
     }
 }
