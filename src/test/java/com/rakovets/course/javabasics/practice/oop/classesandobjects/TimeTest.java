8f954f38afea62d5c98975dc5c22e0139eaaeb78
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-13 20:17:16 +0300

    feat: solution Task01

---
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/TimeTest.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+import java.util.Scanner;
+public class TimeTest {
+    public static void main(String[] args) {
+        int h = Integer.parseInt(args[0]);
+    }
+}
