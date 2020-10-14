f94692d8852d4252eb96a6c756b72466482d9948
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-14 13:00:37 +0300

    feat: solution Point

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Point.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Point.java
@@ -3,25 +3,25 @@ package com.rakovets.course.javabasics.practice.oop.classesandobjects;
 public class Point {
     private int x;
     private int y;
+
     public Point (int x, int y) {
         this.x = x;
         this.y = y;
     }
     public int getX() {
-        return this.x;
+        return x;
     }
     public void setX(int x) {
         this.x = x;
     }
     public int getY() {
-        return this.y;
+        return y;
     }
     public void setY(int y) {
         this.y= y;
     }
-    public int distence(Point pt) {
-        int dis = 0;
-        return dis;
+    public double distance(Point pt) {
+        return Math.sqrt(Math.pow((x- pt.x),2)+Math.pow((y- pt.y),2));
     }
 
 }
