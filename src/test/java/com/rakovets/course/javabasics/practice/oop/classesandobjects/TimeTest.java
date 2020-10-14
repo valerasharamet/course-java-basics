4d5844357b506cef3172fc684a99fba7bc2e9736
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-14 13:00:46 +0300

    feat: solution TimeTest

--- src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/TimeTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/TimeTest.java
@@ -1,9 +1,6 @@
 package com.rakovets.course.javabasics.practice.oop.classesandobjects;
 public class TimeTest {
     public static void main(String[] args) {
-   //     Scanner scanner = new Scanner(System.in);
-     //   int hour = (scanner.nextInt());
-       // int minute = scanner.nextInt();
          Time vrema = new Time (6, 21, 36);
          System.out.println(vrema.getHour() + "/" + vrema.getMinute() + "/" + vrema.getSecond());
          vrema.setHour(7);
