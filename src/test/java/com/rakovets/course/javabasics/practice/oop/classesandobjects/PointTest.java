f767514b0c0866fd27b208338331cc0a54866581
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-14 13:00:27 +0300

    feat: solution PointTest

---
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/PointTest.java
@@ -0,0 +1,13 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class PointTest {
+    public static void main (String[] args) {
+        Point point1 = new Point(2, 3);
+        System.out.println(point1.getX() + "\t" + point1.getY());
+        point1.setX(6);
+        point1.setY(12);
+        System.out.println(point1.getX() + "\t" + point1.getY());
+        Point point2 = new Point(6, 15);
+        System.out.println(point1.distance(point2));
+    }
+}
