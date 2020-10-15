094be749329c4d5851ff41f231eb4856b04f2f49
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-15 03:24:19 +0300

    feat: add solution AtmTest

--- src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
@@ -1,10 +1,16 @@
 package com.rakovets.course.javabasics.practice.oop.classesandobjects;
-
+import java.util.Scanner;
 public class AtmTest {
-    public static void main (String[] args) {
-        Atm atm = new Atm(4,0,0);
-        if(!atm.isPossibleIssue(320))
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Atm atm = new Atm(10, 30, 20);
+        if (!atm.isPossibleIssue(300))
             System.out.println("There are not enough cash in the ATM");
-
+        else {
+            System.out.println("Check option");
+            atm.getOptionsCombinationBanknotes(300);
+            int opt = scanner.nextInt();
+            System.out.println("Take your money " + atm.getCash(opt) + " banknotes");
+        }
     }
 }
