27b765e855907d8423a0e5ee9ed0828668156815
Valerry Sharamet <valerasharamet@gmail.com>
2020-10-24 14:52:35 +0300

    feat: add solution

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
@@ -1,4 +1,8 @@
 package com.rakovets.course.javabasics.practice.strings;
 
 public class StringUtil {
+    String string;
+    public StringUtil (String string) {
+        this.string = string;
+    }
 }
